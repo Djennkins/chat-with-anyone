9333a0932ff2254aba04e36fc2205890091c1b5c
Djennkins <djennkins228@gmail.com>
2024-02-17 23:37:17 +0200

    implemented Authentication Functionality with NextAuth

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -7,7 +7,7 @@ export default function UserAvatar({ name, image, className }: { name: string; i
 		<Avatar className={cn("bg-white text-black ", className)}>
 			{image && <Image src={image} alt={name} width={40} height={40} className="rounded-full" />}
 			<AvatarImage src="https://github.com/shadcn.png" />
-			<AvatarFallback className="dark:bg-white dark:text-black text-lg">
+			<AvatarFallback className="dark:bg-white">
 				{name
 					?.split(" ")
 					.map((n) => n[0])
