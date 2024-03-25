96d6689dce0d349cf66014b5a25b8fc3babb6a44
Djennkins <djennkins228@gmail.com>
2024-03-25 07:18:10 +0200

    initial build of chat page

---
+++ app/(user)/chat/[chatId]/page.tsx
@@ -0,0 +1,10 @@
+export default function ChatsPage() {
+	return (
+		<div>
+			{/* {AdminControls} */}
+			{/* {ChatMembers} */}
+			{/* {ChatMessages} */}
+			{/* {ChatInput} */}
+		</div>
+	);
+}
---
+++ app/(user)/chat/page.tsx
@@ -0,0 +1,19 @@
+import ChatList from "@/components/ChatList";
+
+type Props = {
+	params: {};
+	searchParams: {
+		error: string;
+	};
+};
+
+function ChatsPage({ searchParams: { error } }: Props) {
+	return (
+		<div>
+			{/*Chat Permission chat*/}
+			<ChatList />
+		</div>
+	);
+}
+
+export default ChatsPage;
