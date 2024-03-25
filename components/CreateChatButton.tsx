63290b886cf13d047d025106ec87d4a84ed038e4
Djennkins <djennkins228@gmail.com>
2024-03-25 07:35:59 +0200

    building the CreateChatButton component

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -3,14 +3,48 @@
 import { MessageSquarePlusIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { useSubscriptionStore } from "@/store/store";
+import { useToast } from "./ui/use-toast";
+import LoadingSpinner from "./LoadingSpinner";
+import { v4 as uuidv4 } from "uuid";
 
-function CreateChatButton() {
+function CreateChatButton({ isLarge }: { isLarge?: boolean }) {
+	const { data: session } = useSession();
 	const router = useRouter();
+	const [loading, setLoading] = useState(false);
+	const { toast } = useToast();
+	const subscription = useSubscriptionStore((state) => state.subscription);
 
 	const createNewChat = async () => {
-		//all the logic here
+		if (!session?.user.id) return;
+
+		setLoading(true);
+		toast({
+			title: "Creating new chat... ",
+			description: "Hold tight while we create your new chat...",
+			duration: 3000,
+		});
+
+		//TODO: check if user is pro and limit them creating a  new chat
+		// ...
+		// -------
+
+		const chatId = uuidv4();
+
 		router.push(`chat/abc`);
 	};
+
+	if (isLarge) {
+		return (
+			<div>
+				<Button variant={"default"} onClick={createNewChat}>
+					{loading ? <LoadingSpinner /> : "Creaate a New Chat"}
+				</Button>
+			</div>
+		);
+	}
 
 	return (
 		<Button onClick={createNewChat} variant={"ghost"}>
